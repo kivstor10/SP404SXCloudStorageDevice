296bda9c977443369cc479144fd7ccd54981ad16
kivstor10 <94065305+kivstor10@users.noreply.github.com>
2025-02-21 21:59:43 +0000

    Interfaced SD card using SPI protocol

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,71 @@
-#include <Arduino.h>
+#include "FS.h"
+#include "SD.h"
+#include <SPI.h>
+ 
+#define SD_CS 5
 
+ 
+// Write to the SD card 
+void writeFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Writing file: %s\n", path);
+  File file = fs.open(path, FILE_WRITE);
+  if(!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
+}
+// Append data to the SD card 
+void appendFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Appending to file: %s\n", path);
+  File file = fs.open(path, FILE_APPEND);
+  if(!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
 
 
 void setup() {
   Serial.begin(115200);
+  // Initialize SD card
+  SD.begin(SD_CS);  
+  if(!SD.begin(SD_CS)) {
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+  if(cardType == CARD_NONE) {
+    Serial.println("No SD card attached");
+    return;
+  }
+  Serial.println("Initializing SD card...");
+  if (!SD.begin(SD_CS)) {
+    Serial.println("ERROR - SD card initialisation failed!");
+    return;    // init failed
+  }
+  File file = SD.open("/data.txt");
+  if(!file) {
+    writeFile(SD, "/data.txt", "Card initialised! \r\n");
+  }
+  else {
+    Serial.println("File already exists");  
+  }
+  file.close();
 }
-
 void loop() {
-  Serial.println("Hellow world!");
-  delay(1000);
+
 }
+ 
 
